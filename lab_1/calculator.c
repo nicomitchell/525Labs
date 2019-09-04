75f5348c81dfe470a36e720003cd9293c111ca93
NicoMitchell <nicolasm@ads.isi.edu>
2019-09-04 18:15:02 -0400

    application logic

--- lab_1/calculator.c
+++ lab_1/calculator.c
@@ -8,10 +8,43 @@ extern int remain(int a, int b);
 
 int main()
 {
-    int a = 10;
-    int b = 62;
-    printf("1 + 2 = %d\n", addition(a, b));
-    printf("2 - 1 = %d\n", subtraction(b, a));
-    printf("2 * 2 = %d\n", multiplication(b, b));
-    printf("2 / 1 = %d r %d\n", division(b, a), remain(b, a));
+    while (1)
+    {
+        int op1, op2;
+        char operation;
+        printf("Welcome to the calculator. Please enter the operation you'd like to perform (example: \"1 + 2\")\n");
+        printf("Use only integers for the operands and either +, -, /, or *.\n");
+        scanf("%d %c %d", &op1, &operation, &op2);
+        switch (operation)
+        {
+        case '+':
+            printf(" = %d", addition(op1, op2));
+            break;
+        case '-':
+            printf(" = %d", subtraction(op1, op2));
+            break;
+        case '*':
+            printf(" = %d", multiplication(op1, op2));
+            break;
+        case '/':
+            printf(" = %d r %d", division(op1, op2), remain(op1, op2));
+        default:
+            printf("Unrecognized operation!\n");
+        }
+        printf("Would you like to perform another operation? Y/N\n");
+        char approve;
+        while (1)
+        {
+            scanf("%c", &approve);
+            if (approve == 'Y' || approve == 'N' || approve == 'y' || approve == 'n')
+            {
+                break;
+            }
+            printf("Unrecognized input! Please enter either Y or N.\n");
+        }
+        if (approve == 'N' || approve == 'n')
+        {
+            break;
+        }
+    }
 }
